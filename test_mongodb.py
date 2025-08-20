c7674fceab66714611ae37a21f162d82ac9dc4b8
prc1s <mbfsj2@gmail.com>
2025-08-20 13:36:29 +0300

    Connected to Mongodb

---
+++ test_mongodb.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+from urllib.parse import quote_plus
+
+load_dotenv()
+
+raw_mangodb_username =os.getenv("MANGO_DB_USERNAME")
+raw_mangodb_password = os.getenv("MANGO_DB_PASSWORD")
+
+mangodb_username =quote_plus(raw_mangodb_username)
+mangodb_password = quote_plus(raw_mangodb_password)
+
+from pymongo.mongo_client import MongoClient
+
+uri = f"mongodb+srv://{mangodb_username}:{mangodb_password}@cluster0.yfmjcsk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
